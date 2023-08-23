1cb551d15b021aa7def6a7306966beb78c0380e9
NeoHW <neohaowei@gmail.com>
2023-08-24 04:38:54 +0800

    Initial commit

---
+++ week1/rec1.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int main (void) {
+    char operation;
+    int flag = 1;
+    float firstNum;
+    float secondNum;
+    
+    while(flag) { // while true
+        printf("Please enter an operation (+, -, *, / or q to quit): ");
+        scanf("%c", &operation);
+
+        switch(operation) {
+            case '+':
+                printf("\nEnter the first number: ");
+                scanf("%f", &firstNum);
+                printf("\nEnter the second number: ");
+                scanf("%f", &secondNum);
+                printf("%f + %f = %.2f\n", firstNum, secondNum, firstNum+secondNum);
+                break;
+            case '-':
+                printf("Enter the first number: ");
+                scanf("%f", &firstNum);
+                printf("Enter the second number: ");
+                scanf("%f", &secondNum);
+                printf("%f - %f = %.2f\n", firstNum, secondNum, firstNum-secondNum);
+                break;
+            case '*':
+                printf("Enter the first number: ");
+                scanf("%f", &firstNum);
+                printf("Enter the second number: ");
+                scanf("%f", &secondNum);
+                printf("%f * %f = %.2f\n", firstNum, secondNum, firstNum*secondNum);
+                break;
+            case '/':
+                printf("Enter the first number: ");
+                scanf("%f", &firstNum);
+                printf("Enter the second number: ");
+                scanf("%f", &secondNum);
+                printf("%f / %f = %.2f\n", firstNum, secondNum, firstNum/secondNum);
+                break;
+            case 'q':
+                flag = 0;
+                break;
+            default:
+                printf("Unrecognized operation \n");
+        }   
+    }
+    printf("Bye! \n");
+}
