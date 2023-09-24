f5ac94e36d95ca013a6ff5be3f74cde4b51df78c
NeoHW <neohaowei@gmail.com>
2023-09-24 18:28:23 +0800

    assignment 1

---
+++ assignments/Assignment 1/q3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include "parity.h"
+
+#define BUF_LEN 1024
+
+int main() {
+    int x,temp,ctr;
+    
+    x &= 0;
+    while (x < 5) {
+        ctr /= 2;
+        x++;
+    }
+}
