355eed4c6995d01d38eb7a8c26f0fa19979846b8
NeoHW <neohaowei@gmail.com>
2023-10-11 21:53:30 +0800

    seg fault

--- assignments/Assignment 2/mips.c
+++ assignments/Assignment 2/mips.c
@@ -57,25 +57,25 @@ int32_t Memory(uint32_t Address, int32_t WrData, bool MemRead, bool MemWrite) {
 }
 #endif
 /******************************************************************************
- *      DO NOT MIDIFY THE CODE ABOVE
+ *      DO NOT MODIFY THE CODE ABOVE
  ******************************************************************************/
 
 // Here starts your code for Assignment 2 Part A
 // If you need to define some macros, you can do so below this comment.
 
 #ifndef ASSIGNMENT2_QUESTION1A
-#define SELECT_INPUT(ctrl, in0, in1) ((ctrl) ? (in1) : (in0))
+#define SELECT_INPUT() return (ctrl) ? (in1) : (in0)
 
 uint8_t mux_u8(bool ctrl, uint8_t in0, uint8_t in1) {
-    return SELECT_INPUT(ctrl, in0, in1);
+    SELECT_INPUT();
 }
 
 uint32_t mux_u32(bool ctrl, uint32_t in0, uint32_t in1) {
-    return SELECT_INPUT(ctrl, in0, in1);
+    SELECT_INPUT();
 }
 
 int32_t mux_i32(bool ctrl, int32_t in0, int32_t in1) {
-    return SELECT_INPUT(ctrl, in0, in1);
+    SELECT_INPUT();
 }
 
 #endif  // End of Assignment 2, Question 1a
@@ -91,19 +91,19 @@ struct instr {
 */
 
 void decode(uint32_t in, struct instr* insn) {
-    if (in >> 26 == 0) { // in | 0xFC000000
+    if (in >> 26 == 0) { // in & 0xFC000000
         insn->opcode = in >> 26;
-        insn->rs = in | 0x03E00000 >> 21;
-        insn->rt = in | 0x001F0000 >> 16;
-        insn->rd = in | 0x0000F800 >> 10;
-        insn->shamt = in | 0x000007C0 >> 6;
-        insn->funct = in | 0x0000003F;
+        insn->rs = (in & 0x03E00000) >> 21;
+        insn->rt = (in & 0x001F0000) >> 16;
+        insn->rd = (in & 0x0000F800) >> 11;
+        insn->shamt = (in & 0x000007C0) >> 6;
+        insn->funct = (in & 0x0000003F);
     } else {
         insn->opcode = in >> 26;
-        insn->rs = in | 0x03E00000 >> 21;
-        insn->rt = in | 0x001F0000 >> 16;
-        insn->immed = in | 0x0000FFFF;
-        // insn->address = in | 0x03FFFFFF
+        insn->rs = (in & 0x03E00000) >> 21;
+        insn->rt = (in & 0x001F0000) >> 16;
+        insn->immed = in & 0x0000FFFF;
+        insn->address = in & 0x03FFFFFF;    
     }
 }
 
@@ -186,6 +186,7 @@ uint8_t ALUControl(uint8_t ALUOp, uint8_t funct) {
 		   return 2; // Ask ALU to add (lw/sw)
 		if (ALUOp == 1)
 		   return 6; // Ask ALU to sub (beq)
+        return -1;  // to solve warning
 	}
 }
 
@@ -196,22 +197,22 @@ uint8_t ALUControl(uint8_t ALUOp, uint8_t funct) {
 int32_t ALU(int32_t in0, int32_t in1, uint8_t ALUControl, bool* ALUiszero) {
     int32_t result;
 	switch (ALUControl) {
-		case 0:
+		case 0: // and
 			result = in0 & in1;
 			break;
-		case 1:
+		case 1: // or
 			result = in0 | in1;
 			break;
-		case 2:
+		case 2: // add
 			result = in0 + in1;
 			break;
-		case 6:
+		case 6: // sub
 			result = in0 - in1;
 			break;
-		case 7:
+		case 7: // slt
 			result = (int32_t)(in0 < in1);
 			break;
-		case 12:
+		case 12: // nor
 			result = ~(in0 | in1);
 			break;
 	}
@@ -224,7 +225,62 @@ int32_t ALU(int32_t in0, int32_t in1, uint8_t ALUControl, bool* ALUiszero) {
 #ifndef ASSIGNMENT2_QUESTION3
 
 void execute(uint32_t insn) {
-    // TODO: Implement execute
+    // pointers to the control signals    
+    bool* RegDst = &_RegDst;
+    bool* ALUSrc = &_ALUSrc;
+    bool* MemtoReg = &_MemtoReg;
+    bool* RegWrite = &_RegWrite;
+    bool* MemRead = &_MemRead;
+    bool* MemWrite = &_MemWrite;
+    bool* Branch = &_Branch;
+    uint8_t* ALUOp = &_ALUOp;
+    uint8_t* ALUCtrl = &_ALUCtrl;
+    bool* ALUiszero = malloc(1);
+    
+    // decode stage
+    struct instr* i;
+    decode(insn, i);
+
+    // Generate the control Signals
+    Control(i->opcode, RegDst, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, ALUOp);
+    *ALUCtrl = ALUControl(*ALUOp, i->funct);
+    
+    // registers
+    uint8_t RR1 = i->rs;
+    uint8_t RR2 = i->rt;
+    uint8_t WR = mux_u8(*RegDst,i->rt,i->rd);
+    // int32_t WD; // to be computed later from MemToReg
+    int32_t *RD1 = &_RF[RR1];
+    int32_t *RD2 = &_RF[RR2];
+
+    // operands 1 & 2 for ALU
+    int32_t ALUop1 = *RD1;
+    int32_t ALUop2 = mux_i32(ALUSrc, *RD2, i->immed);
+
+    // ALU stage
+    uint32_t result = ALU(ALUop1,ALUop2, *ALUCtrl, ALUiszero);
+
+    // PCSrc control signal
+    bool PCSrc = *Branch & *ALUiszero;
+    // calculating branch target address / Updating PC
+    uint32_t PCSrcInputOne = _PC + 4;
+    uint32_t PCSrcInputTwo = (_PC + 4) + (i->immed << 2);
+    uint32_t PCaddress = mux_u32(PCSrc, PCSrcInputOne, PCSrcInputTwo);
+
+    // Memory stage
+    uint32_t memStageAddress = result;
+    uint32_t WriteData = *RD2;
+    uint32_t memReadData = Memory(memStageAddress, WriteData, *MemRead, *MemWrite);
+
+    // Writeback
+    uint32_t WD = mux_u32(*MemtoReg, result, memReadData);
+
+    // update regfile
+    RegFile(RR1, RR2, WR, WD, RD1, RD2, *RegWrite);
+
+    // Set PC address to target PC address
+    _PC = PCaddress;
+    
 }
 
 #endif  // End of Assignment 2, Question 3
